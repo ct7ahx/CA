5c5c935b4fcea140a7613a77977db2094b133973
MrD2 <dy090.guerra@gmail.com>
2014-05-08 02:29:11 +0100

    RGB full algorithm not working

--- RGB_Center_Of_Mass/src/RGB_Center_Of_Mass.cpp
+++ RGB_Center_Of_Mass/src/RGB_Center_Of_Mass.cpp
@@ -1,10 +1,18 @@
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include <iostream>
+#include <fstream>
+#include <string>
 #include <stdio.h>
 #include <stdlib.h>
 
-#define BOUNDING_AREA 2000		//Min area to consider for analysis
+#define BOUNDING_AREA 2500		//Min area to consider for analysis
+#define BINARY_THRESHOLD 50		//Binary Threshold for filter binary calculation
+#define RESIZE_XY_SIZE Size(50,50)
+
+#define IMAGE_COMPARISON_THRESHOLD 750000.0
+
+#define SMALL_NAME_BYTES	50
 
 using namespace cv;
 using namespace std;
@@ -19,7 +27,15 @@ typedef struct _canny_def{
 	char filter_name[20];
 	vector<Point2i> filter_mc_points;
 }define_canny;
-define_canny struct_canny[3];
+define_canny struct_canny[3], *pcanny_struct_canny = struct_canny;
+
+typedef struct _compare_signal{
+	Mat ref;
+	string name;
+}define_compare_signal;
+vector<define_compare_signal> compare_signals;
+
+vector<Mat> possible_signals;
 
 ///Point2i coord = struct_canny[0].filter_mc_points.front();
 Mat src;
@@ -30,23 +46,53 @@ int max_thresh = 255;
 RNG rng(12345);
 
 /// Function header
-void thresh_callback(int, void* argv);
+void find_possible_signals(int, void* argv);
+//void test_compare_for_signal(void*arg);
 
 /** @function main */
 int main( int argc, char** argv )
 {
-		VideoCapture capture;
+	//Get Signals and Resize
+	const char *lista_sinais = "Signs/Signals.txt";	//caminho que contem todos os sinais para contra-teste
+	fstream fp;
+	fp.open(lista_sinais,std::fstream::in);
+	Mat aux, resize_aux;
+	string name, append = "Signs/";;
+	//if(fp.eof) return "error";
+	while(!fp.eof()){
+		define_compare_signal aux_compare_signal;
+		fp >> name;
+		name = (append + name);
+		aux_compare_signal.name = name;
+		aux = imread(aux_compare_signal.name.c_str(),CV_LOAD_IMAGE_COLOR);
+		resize(aux, resize_aux, RESIZE_XY_SIZE);
+		resize_aux.copyTo(aux_compare_signal.ref);
+		compare_signals.push_back(aux_compare_signal);
+	}
+	fp.close();//O PROBLEMA COM AS SETAS E DEPOIS E A CENA DE SUBTRAIR
+	/*define_compare_signal cmp_signal;
+	while(!compare_signals.empty()){
+		cmp_signal = compare_signals.back();
+		imshow(cmp_signal.name,cmp_signal.ref);
+		compare_signals.pop_back();
+	}*/
+	waitKey(0);
+	VideoCapture capture;
 
 	///open capture object at location zero (default location for webcam)
-		capture.open(0);
+		//capture.open(0);
 
 	///set height and width of capture frame
-		capture.set(CV_CAP_PROP_FRAME_WIDTH,FRAME_WIDTH);
-		capture.set(CV_CAP_PROP_FRAME_HEIGHT,FRAME_HEIGHT);
+		//capture.set(CV_CAP_PROP_FRAME_WIDTH,FRAME_WIDTH);
+		//capture.set(CV_CAP_PROP_FRAME_HEIGHT,FRAME_HEIGHT);
+	Mat Aux0;
+	Mat Aux1;
+	Mat Aux2;
+
 	while(1){
 		/// Load source image and convert it to gray
 		 //src = imread( "Signs/bus A4.png",CV_LOAD_IMAGE_COLOR);
-		capture.read(src);
+		//capture.read(src);
 
 		///Get image from camera and split(or to get patern signals)
 		src = imread("Test_Signals/10.jpg",CV_LOAD_IMAGE_COLOR);
@@ -74,12 +120,12 @@ int main( int argc, char** argv )
 	//imshow("G", src_bgr[1]);
 	//imshow("R", src_bgr[2]);
 
-	threshold(struct_canny[0].src_gray,struct_canny[0].bin, 50, 255,THRESH_BINARY);
-	threshold(struct_canny[1].src_gray,struct_canny[1].bin, 50, 255,THRESH_BINARY);
-	threshold(struct_canny[2].src_gray,struct_canny[2].bin, 50, 255,THRESH_BINARY);
-	Mat Aux0 = struct_canny[0].bin - (struct_canny[0].bin & struct_canny[1].bin & struct_canny[2].bin);
-	Mat Aux1 = struct_canny[1].bin - (struct_canny[0].bin & struct_canny[1].bin & struct_canny[2].bin);
-	Mat Aux2 = struct_canny[2].bin - (struct_canny[0].bin & struct_canny[1].bin & struct_canny[2].bin);
+	threshold(struct_canny[0].src_gray,struct_canny[0].bin, BINARY_THRESHOLD, 255,THRESH_BINARY);
+	threshold(struct_canny[1].src_gray,struct_canny[1].bin, BINARY_THRESHOLD, 255,THRESH_BINARY);
+	threshold(struct_canny[2].src_gray,struct_canny[2].bin, BINARY_THRESHOLD, 255,THRESH_BINARY);
+	Aux0 = struct_canny[0].bin - (struct_canny[0].bin & struct_canny[1].bin & struct_canny[2].bin);
+	Aux1 = struct_canny[1].bin - (struct_canny[0].bin & struct_canny[1].bin & struct_canny[2].bin);
+	Aux2 = struct_canny[2].bin - (struct_canny[0].bin & struct_canny[1].bin & struct_canny[2].bin);
 	struct_canny[0].bin = Aux0;
 	struct_canny[1].bin = Aux1;
 	struct_canny[2].bin = Aux2;
@@ -89,37 +135,62 @@ int main( int argc, char** argv )
 	//imshow("RAND", Aux2);
 
 		///Aply Canny to each filter RGB (CAN SPLIT INTO THREADS)
-		createTrackbar( " Canny thresh:", "Source", &thresh, max_thresh);//, thresh_callback ); //define threshold
-		//Aply for each -> thresh_callback(num_args,Mat *src_grey,const char <window name>)
-		thresh_callback( 1, (void *) &struct_canny[0]);
-		thresh_callback( 1, (void *) &struct_canny[1]);
-		thresh_callback( 1, (void *) &struct_canny[2]);
+		createTrackbar( " Canny thresh:", "Source", &thresh, max_thresh);//, find_possible_signals ); //define threshold
+		//Aply for each -> find_possible_signals(num_args,Mat *src_grey,const char <window name>)
+		find_possible_signals( 1, (void *) &struct_canny[0]);
+		find_possible_signals( 1, (void *) &struct_canny[1]);
+		find_possible_signals( 1, (void *) &struct_canny[2]);
 
-		cout << struct_canny[0].filter_mc_points;
+		Scalar min_sums;			//vector<double> 4 channels
+		Mat test_matrix;							//test matrix to run tests
+		Mat aux_test_matrix;						//test matrix to suffeer operations
+		define_compare_signal *ppossible_signal;	//pointer to possible signal, with lowest sumed value
+		Scalar sum_color;
+		while(!possible_signals.empty()){
+			test_matrix = possible_signals.back();
+			for(unsigned num_tests=0; num_tests<compare_signals.size();num_tests++){
+				min_sums = Scalar(DBL_MAX,DBL_MAX,DBL_MAX);				//reset values
+				test_matrix.copyTo(aux_test_matrix);					//copy image to new test slate
+				aux_test_matrix -= compare_signals[num_tests].ref;		//make diference operation
+				sum_color = sum(aux_test_matrix);						//sum total pix value by channel
+				imshow("Result",aux_test_matrix);						//print (debug)
+				if(sum_color[0]<=min_sums[0] || sum_color[1]<=min_sums[1] || sum_color[2]<=min_sums[2]){//Test if new lowest value
+					min_sums = sum_color;
+					ppossible_signal = &compare_signals[num_tests];
+				}
+				else; //image had not so good oods
+				cout << sum_color[0] << " " << sum_color[1] << " " << sum_color[2] << endl;
+				waitKey(2000);
+			}
+			if((min_sums[0]+min_sums[1]+min_sums[2]) <= IMAGE_COMPARISON_THRESHOLD){
+				//Show text in image to help show and see if its right
+				cout << (min_sums[0]+min_sums[1]+min_sums[2]) << endl;
+				imshow(ppossible_signal->name,ppossible_signal->ref);
+			}
+			possible_signals.pop_back();
+			waitKey(4000);
+		}
+
+		//cout << struct_canny[0].filter_mc_points;
 		//Point2i ponto = struct_canny[0].filter_mc_points.front(); //tirar ponto
-		//TODO:PERCORRER VECTOR COM TEST obtido
 		//struct_canny[0].filter_mc_points.front()
-		struct_canny[0].filter_mc_points.clear();
 		waitKey(0);
 	}
   return(0);
 }
 
-/** @function thresh_callback */
-void thresh_callback(int num, void *argv)
+/** @function find_possible_signals */
+void find_possible_signals(int num, void *argv)
 {
   Mat canny_output;
   vector<vector<Point> > contours;
   vector<Vec4i> hierarchy;
-  char buf[20];
-Mat bin;
+  Mat bin;
   define_canny *data = (define_canny *) argv;
 
   /// Detect edges using canny
   //Canny( data->src_gray, canny_output, thresh, thresh*2, 3 );
   Canny( data->bin, canny_output, thresh, thresh*2, 3 );
-  //threshold(data->src_gray,bin, 70, 255,THRESH_BINARY);
-    //imshow( data->filter_name, bin);
 
   /// Find contours
   findContours( canny_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
@@ -136,63 +207,24 @@ Mat bin;
        minEnclosingCircle( (Mat)contours_poly[i], center[i], radius[i] );
      }
 
-
   /// Draw polygonal contour + bonding rects + circles
   Mat drawing = Mat::zeros( canny_output.size(), CV_8UC3 );
-  for( int i = 0; i< contours.size(); i++ )
-     {char num[6];
+  Mat aux_resize;
+  for( int i = 0; i< contours.size(); i++ ){
 		if(boundRect[i].area()>BOUNDING_AREA){
 			Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
 			drawContours( drawing, contours_poly, i, color, 1, 8, vector<Vec4i>(), 0, Point() );
 			rectangle( drawing, boundRect[i].tl(), boundRect[i].br(), color, 2, 8, 0 );
 			cout << boundRect[i] <<endl;
-			strcpy(buf,"Croped ");strcat(buf,data->filter_name);sprintf(num,"%d",i);strcat(buf,num);
-			Mat croppedImage = src(boundRect[i]);
-			imshow(buf,croppedImage);
+			//strcpy(buf,"Croped ");strcat(buf,data->filter_name);sprintf(num,"%d",i);strcat(buf,num);
+			resize(src(boundRect[i]),aux_resize,RESIZE_XY_SIZE);
+			possible_signals.push_back(aux_resize);
              //circle( drawing, center[i], (int)radius[i], color, 2, 8, 0 );
 		}
      }
 
   /// Show in a window
-  namedWindow( "Contours", CV_WINDOW_AUTOSIZE );
-  strcpy(buf,"Contours");strcat(buf,data->filter_name);
+  //namedWindow( "Contours", CV_WINDOW_AUTOSIZE );
+  //strcpy(buf,"Contours");strcat(buf,data->filter_name);
   //imshow(buf, drawing );
-
-  /// Get the moments
-  vector<Moments> mu(contours.size() );
-  for( int i = 0; i < contours.size(); i++ )
-     { mu[i] = moments( contours[i], false );}
-
-  ///  Get the mass centers:
-  vector<Point2f> mc( contours.size() );
-  for( int i = 0; i < contours.size(); i++ )
-     { mc[i] = Point2f( mu[i].m10/mu[i].m00 , mu[i].m01/mu[i].m00 );
-     //SO ESTE E PRECISO, PARTE DE BAIXO E DESCARTAVEL
-     data->filter_mc_points.push_back(Point2i((int) mu[i].m10/mu[i].m00,(int) mu[i].m01/mu[i].m00 ));}
-
-  /// Draw contours (Nao e preciso isto e treta!!!)
-drawing = Mat::zeros( canny_output.size(), CV_8UC3 );
- /* for( int i = 0; i< contours.size(); i++ )
-     {
-       Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
-       drawContours( drawing, contours, i, color, 2, 8, hierarchy, 0, Point() );
-       circle( drawing, mc[i], 4, color, -1, 8, 0 );
-     }*/
-  /////ATEE AQUI
-
-  /// Calculate the area with the moments 00 and compare with the result of the OpenCV function
-  //printf("\t Info: Area and Contour Length \n");
-  for( int i = 0; i< contours.size(); i++ )
-     {
-       //printf(" * Contour[%d] - Area (M_00) = %.2f - Area OpenCV: %.2f - Length: %.2f \n", i, mu[i].m00, contourArea(contours[i]), arcLength( contours[i], true ) );
-       Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
-     drawContours( drawing, contours, i, color, 2, 8, hierarchy, 0, Point() );
-       circle( drawing, mc[i], 3, color, -1, 8, 0 );
-     sprintf(buf,"%lf %lf",mc[i].x,mc[i].y);
-    //  putText(drawing,buf,mc[i],FONT_HERSHEY_SIMPLEX,0.5,color);
-     }
-
-  /// Show in a window
- // namedWindow( data->filter_name, CV_WINDOW_AUTOSIZE );
-  //imshow( data->filter_name, drawing );
 }

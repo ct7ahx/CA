80eabe139dabf84ea3ba99cb2c430444b30eaed3
MrD2 <dy090.guerra@gmail.com>
2014-05-11 20:06:08 +0100

    Compare image and ORB juntos, ainda com o problema de só avaliar a primeira imagem

--- RGB_Center_Of_Mass/src/RGB_Center_Of_Mass.cpp
+++ RGB_Center_Of_Mass/src/RGB_Center_Of_Mass.cpp
@@ -1,230 +1,266 @@
-#include "opencv2/highgui/highgui.hpp"
-#include "opencv2/imgproc/imgproc.hpp"
+//TODO: MUDAR PARAMETROS DA IMAGEM esta a detectar poucos pontos
+//tentar diminuir imagem ao minimo
+
+
+#include <iostream>
+#include <vector>
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/nonfree/features2d.hpp>
+#include <opencv2/features2d/features2d.hpp>
+#include "opencv2/nonfree/nonfree.hpp"
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <stdio.h>
 #include <stdlib.h>
 
-#define BOUNDING_AREA 2500		//Min area to consider for analysis
+//DEBUG
+#define RAW_SIGNAL	true
+#define DEBUG 		true
+
+#define BOUNDING_AREA 1500		//Min area to consider for analysis
 #define BINARY_THRESHOLD 50		//Binary Threshold for filter binary calculation
-#define RESIZE_XY_SIZE Size(50,50)
+#define RESIZE_XY_SIZE Size(300,300)
+#define CANNY_THRESHOLD 200
 
-#define IMAGE_COMPARISON_THRESHOLD 750000.0
-
+#define TINY_NAME_BYTES		10
 #define SMALL_NAME_BYTES	50
 
 using namespace cv;
 using namespace std;
 
+// set parameters
+
+int numKeyPoints = 2500;
+float distThreshold = 30.0;
+
 //default capture width and height
+#define CAMERA_FEED 1
 const int FRAME_WIDTH = 640;
 const int FRAME_HEIGHT = 480;
 
-typedef struct _canny_def{
+typedef struct _define_raw_test{
 	Mat src_gray;
+
 	Mat bin;
-	char filter_name[20];
+	Mat equalized_histogram;
+	string filter_name;
 	vector<Point2i> filter_mc_points;
-}define_canny;
-define_canny struct_canny[3], *pcanny_struct_canny = struct_canny;
+}define_raw_test;
+define_raw_test src_filtered[3];
 
 typedef struct _compare_signal{
 	Mat ref;
 	string name;
+	Mat src_gray;
+	vector<KeyPoint> keypoints;
+	Mat descriptors;
 }define_compare_signal;
 vector<define_compare_signal> compare_signals;
 
 vector<Mat> possible_signals;
 
-///Point2i coord = struct_canny[0].filter_mc_points.front();
+//Camera Image
 Mat src;
 Mat src_bgr[3];
-Mat test_gray;
-int thresh = 60;
-int max_thresh = 255;
-RNG rng(12345);
+Mat src_gray;
+vector<KeyPoint> keypoints;
+Mat descriptors;
+int thresh = CANNY_THRESHOLD;
+
+//Other
+Mat aux_mat[3];
+Mat src_puro;
 
 /// Function header
-void find_possible_signals(int, void* argv);
+void find_possible_signals(int num, void *argv);
 //void test_compare_for_signal(void*arg);
 
 /** @function main */
 int main( int argc, char** argv )
 {
+#if(RAW_SIGNAL == true)
+	VideoCapture capture;
+	capture.open(CAMERA_FEED);
+	capture.set(CV_CAP_PROP_FRAME_WIDTH,FRAME_WIDTH);
+	capture.set(CV_CAP_PROP_FRAME_HEIGHT,FRAME_HEIGHT);
+#endif
 	//Get Signals and Resize
 	const char *lista_sinais = "Signs/Signals.txt";	//caminho que contem todos os sinais para contra-teste
 	fstream fp;
 	fp.open(lista_sinais,std::fstream::in);
 	Mat aux, resize_aux;
+	Mat Aux[3];
 	string name, append = "Signs/";;
-	//if(fp.eof) return "error";
+	Ptr<FeatureDetector> detector(new OrbFeatureDetector(5000));
+	Ptr<DescriptorExtractor> extractor(new OrbDescriptorExtractor(5000));
+	BFMatcher matcher(NORM_HAMMING2);
+	vector<vector<DMatch> > matches;
+	Mat img_matches;
+	Mat testest;
+	int best_match;
+	int number_best_match;
+
 	while(!fp.eof()){
 		define_compare_signal aux_compare_signal;
 		fp >> name;
 		name = (append + name);
 		aux_compare_signal.name = name;
-		aux = imread(aux_compare_signal.name.c_str(),CV_LOAD_IMAGE_COLOR);
-		resize(aux, resize_aux, RESIZE_XY_SIZE);
+		aux = imread(aux_compare_signal.name.c_str(),CV_LOAD_IMAGE_GRAYSCALE);
+		equalizeHist(aux, aux);
+		resize(aux, resize_aux,RESIZE_XY_SIZE);
 		resize_aux.copyTo(aux_compare_signal.ref);
 		compare_signals.push_back(aux_compare_signal);
 	}
-	fp.close();//O PROBLEMA COM AS SETAS E DEPOIS E A CENA DE SUBTRAIR
-	/*define_compare_signal cmp_signal;
-	while(!compare_signals.empty()){
-		cmp_signal = compare_signals.back();
-		imshow(cmp_signal.name,cmp_signal.ref);
-		compare_signals.pop_back();
-	}*/
-	waitKey(0);
-	VideoCapture capture;
+	fp.close();
+	for(unsigned int num_signals = 0; num_signals<compare_signals.size();num_signals++){
+		//GET COMPARE IMAGE KEYPOINTS AND DESCRIPTORS
+		blur(compare_signals[num_signals].ref,compare_signals[num_signals].src_gray, Size(3,3) );
+		detector->detect(compare_signals[num_signals].src_gray,compare_signals[num_signals].keypoints);
+		extractor->compute(compare_signals[num_signals].src_gray,compare_signals[num_signals].keypoints,compare_signals[num_signals].descriptors);
+#if(false)//DEBUG == true)
+		imshow("descriptors",compare_signals[num_signals].descriptors);
+		compare_signals[num_signals].ref.copyTo(testest);
+		drawKeypoints(testest,compare_signals[num_signals].keypoints,testest);
+		imshow("Keypoints",testest);
+#endif
+		//waitKey(1);
 
-	///open capture object at location zero (default location for webcam)
-		//capture.open(0);
-
-	///set height and width of capture frame
-		//capture.set(CV_CAP_PROP_FRAME_WIDTH,FRAME_WIDTH);
-		//capture.set(CV_CAP_PROP_FRAME_HEIGHT,FRAME_HEIGHT);
-	Mat Aux0;
-	Mat Aux1;
-	Mat Aux2;
-
-	while(1){
-		/// Load source image and convert it to gray
-		 //src = imread( "Signs/bus A4.png",CV_LOAD_IMAGE_COLOR);
-		//capture.read(src);
-
-		///Get image from camera and split(or to get patern signals)
-		src = imread("Test_Signals/10.jpg",CV_LOAD_IMAGE_COLOR);
-		//src = imread("Test_Signals/garrafa.jpg",CV_LOAD_IMAGE_COLOR); //RESOLUCAO MUITO GRANDE, USE AT UR OWN RISK!!!
-		//src = imread("Signs/yellow_left_arrow.png",CV_LOAD_IMAGE_COLOR);
-		split(src,src_bgr);
-		/// Convert each filter to gray and blur it(CAN SPLIT INTO THREADS
-		//EDIT: As the images are in RGB separated filters, it counts as grayscale images
-		struct_canny[0].src_gray=src_bgr[0];
-		struct_canny[1].src_gray=src_bgr[1];
-		struct_canny[2].src_gray=src_bgr[2];
-		blur(struct_canny[0].src_gray,struct_canny[0].src_gray, Size(3,3) );
-		strcpy(struct_canny[0].filter_name,"CannyB");
-		blur(struct_canny[1].src_gray,struct_canny[1].src_gray, Size(3,3) );
-		strcpy(struct_canny[1].filter_name,"CannyG");
-		blur(struct_canny[2].src_gray,struct_canny[2].src_gray, Size(3,3) );
-		strcpy(struct_canny[2].filter_name,"CannyR");
-
-	/// Create and show DEFAULT IMAGE
-	char* source_window = "Source";
-	namedWindow( source_window, CV_WINDOW_AUTOSIZE );
-	imshow( source_window, src );
-	///DEBUG RGB IMAGE
-	//imshow("B", src_bgr[0]);
-	//imshow("G", src_bgr[1]);
-	//imshow("R", src_bgr[2]);
-
-	threshold(struct_canny[0].src_gray,struct_canny[0].bin, BINARY_THRESHOLD, 255,THRESH_BINARY);
-	threshold(struct_canny[1].src_gray,struct_canny[1].bin, BINARY_THRESHOLD, 255,THRESH_BINARY);
-	threshold(struct_canny[2].src_gray,struct_canny[2].bin, BINARY_THRESHOLD, 255,THRESH_BINARY);
-	Aux0 = struct_canny[0].bin - (struct_canny[0].bin & struct_canny[1].bin & struct_canny[2].bin);
-	Aux1 = struct_canny[1].bin - (struct_canny[0].bin & struct_canny[1].bin & struct_canny[2].bin);
-	Aux2 = struct_canny[2].bin - (struct_canny[0].bin & struct_canny[1].bin & struct_canny[2].bin);
-	struct_canny[0].bin = Aux0;
-	struct_canny[1].bin = Aux1;
-	struct_canny[2].bin = Aux2;
-
-	//imshow("BAND", Aux0);
-	//imshow("GAND", Aux1);
-	//imshow("RAND", Aux2);
-
-		///Aply Canny to each filter RGB (CAN SPLIT INTO THREADS)
-		createTrackbar( " Canny thresh:", "Source", &thresh, max_thresh);//, find_possible_signals ); //define threshold
-		//Aply for each -> find_possible_signals(num_args,Mat *src_grey,const char <window name>)
-		find_possible_signals( 1, (void *) &struct_canny[0]);
-		find_possible_signals( 1, (void *) &struct_canny[1]);
-		find_possible_signals( 1, (void *) &struct_canny[2]);
-
-		Scalar min_sums;			//vector<double> 4 channels
-		Mat test_matrix;							//test matrix to run tests
-		Mat aux_test_matrix;						//test matrix to suffeer operations
-		define_compare_signal *ppossible_signal;	//pointer to possible signal, with lowest sumed value
-		Scalar sum_color;
-		while(!possible_signals.empty()){
-			test_matrix = possible_signals.back();
-			for(unsigned num_tests=0; num_tests<compare_signals.size();num_tests++){
-				min_sums = Scalar(DBL_MAX,DBL_MAX,DBL_MAX);				//reset values
-				test_matrix.copyTo(aux_test_matrix);					//copy image to new test slate
-				aux_test_matrix -= compare_signals[num_tests].ref;		//make diference operation
-				sum_color = sum(aux_test_matrix);						//sum total pix value by channel
-				imshow("Result",aux_test_matrix);						//print (debug)
-				if(sum_color[0]<=min_sums[0] || sum_color[1]<=min_sums[1] || sum_color[2]<=min_sums[2]){//Test if new lowest value
-					min_sums = sum_color;
-					ppossible_signal = &compare_signals[num_tests];
-				}
-				else; //image had not so good oods
-				cout << sum_color[0] << " " << sum_color[1] << " " << sum_color[2] << endl;
-				waitKey(2000);
-			}
-			if((min_sums[0]+min_sums[1]+min_sums[2]) <= IMAGE_COMPARISON_THRESHOLD){
-				//Show text in image to help show and see if its right
-				cout << (min_sums[0]+min_sums[1]+min_sums[2]) << endl;
-				imshow(ppossible_signal->name,ppossible_signal->ref);
-			}
-			possible_signals.pop_back();
-			waitKey(4000);
-		}
-
-		//cout << struct_canny[0].filter_mc_points;
-		//Point2i ponto = struct_canny[0].filter_mc_points.front(); //tirar ponto
-		//struct_canny[0].filter_mc_points.front()
-		waitKey(0);
 	}
-  return(0);
+	while(1){
+		//Sharpen image
+		Mat image;
+		capture.read(src);
+		src_puro = src;
+		GaussianBlur(src,image,cv::Size(0, 0), 3);
+		//imshow("Blur",image);
+		addWeighted(src, 1.5, image, -0.8, 0, image); //imagem peso imagem peso
+		//imshow("sharpen",image);
+		src=image;
+
+
+		split(src,src_bgr);
+		src_filtered[0].src_gray=src_bgr[0];
+		src_filtered[1].src_gray=src_bgr[1];
+		src_filtered[2].src_gray=src_bgr[2];
+		blur(src_filtered[0].src_gray,src_filtered[0].src_gray, Size(3,3) );
+		src_filtered[0].filter_name = "CannyB";
+		blur(src_filtered[1].src_gray,src_filtered[1].src_gray, Size(3,3) );
+		src_filtered[1].filter_name = "CannyG";
+		blur(src_filtered[2].src_gray,src_filtered[2].src_gray, Size(3,3) );
+		src_filtered[2].filter_name = "CannyR";
+
+		//imshow("GREYSCALEB",src_filtered[0].src_gray);
+		//imshow("GREYSCALEG",src_filtered[1].src_gray);
+		//imshow("GREYSCALER",src_filtered[2].src_gray);
+
+		threshold(src_filtered[0].src_gray,src_filtered[0].bin, BINARY_THRESHOLD, 255,THRESH_BINARY);
+		threshold(src_filtered[1].src_gray,src_filtered[1].bin, BINARY_THRESHOLD, 255,THRESH_BINARY);
+		threshold(src_filtered[2].src_gray,src_filtered[2].bin, BINARY_THRESHOLD, 255,THRESH_BINARY);
+		Aux[0] = src_filtered[0].bin - (src_filtered[0].bin & src_filtered[1].bin & src_filtered[2].bin);
+		Aux[1] = src_filtered[1].bin - (src_filtered[0].bin & src_filtered[1].bin & src_filtered[2].bin);
+		Aux[2] = src_filtered[2].bin - (src_filtered[0].bin & src_filtered[1].bin & src_filtered[2].bin);
+		src_filtered[0].bin = Aux[0];
+		src_filtered[1].bin = Aux[1];
+		src_filtered[2].bin = Aux[2];
+		//imshow("BINB",Aux[0]);
+		//imshow("BING",Aux[1]);
+		//imshow("BINR",Aux[2]);
+		//waitKey(1);
+		char* source_window = "Source";
+		namedWindow( source_window, CV_WINDOW_AUTOSIZE );
+		imshow( source_window, src );
+		createTrackbar( " Canny thresh:", "Source", &thresh, 255);//, find_possible_signals ); //define threshold
+		//Aply for each -> find_possible_signals(num_args,Mat *src_grey,const char <window name>)
+		find_possible_signals( 1, (void *) &src_filtered[0]);
+		find_possible_signals( 1, (void *) &src_filtered[1]);
+		find_possible_signals( 1, (void *) &src_filtered[2]);
+
+		Mat test_src,test_gray;
+		while(!possible_signals.empty()){
+			test_src = possible_signals.back();
+			cvtColor(test_src,test_gray,CV_RGB2GRAY);
+			//equalizeHist(test_gray, test_gray);
+			//			blur(test_gray,test_gray, Size(3,3) );
+			detector->detect(test_gray,keypoints);
+			extractor->compute(test_gray,keypoints,descriptors);
+			best_match=0;
+			number_best_match=5;
+			for(unsigned int num_signals = 0,matchCount=0; num_signals<compare_signals.size();num_signals++){
+				matcher.knnMatch(descriptors,compare_signals[0].descriptors,matches,1);
+				matchCount=0;
+				for (int n=0; n<matches.size(); ++n) {
+					if (matches[n].size() > 0){
+						if (matches[n][0].distance > distThreshold){
+							matches[n].erase(matches[n].begin());
+						}else{
+							++matchCount;
+						}
+
+					}
+				}
+				cout << "MATCH COUNT " << matchCount << endl;
+				if(matchCount>best_match){
+					best_match = matchCount;
+					number_best_match = num_signals;
+				}
+			}
+			drawMatches(test_gray, keypoints, compare_signals[number_best_match].ref, compare_signals[number_best_match].keypoints, matches, img_matches );
+			imshow("IMAGE MAtCHES",img_matches);
+			waitKey(1);
+
+			possible_signals.pop_back();
+		}
+	}
+	destroyAllWindows();
+	return 0;
 }
+
+
 
 /** @function find_possible_signals */
 void find_possible_signals(int num, void *argv)
 {
-  Mat canny_output;
-  vector<vector<Point> > contours;
-  vector<Vec4i> hierarchy;
-  Mat bin;
-  define_canny *data = (define_canny *) argv;
+	Mat canny_output;
+	vector<vector<Point> > contours;
+	vector<Vec4i> hierarchy;
+	Mat bin;
+	define_raw_test *data = (define_raw_test *) argv;
+	RNG rng(12345);
 
-  /// Detect edges using canny
-  //Canny( data->src_gray, canny_output, thresh, thresh*2, 3 );
-  Canny( data->bin, canny_output, thresh, thresh*2, 3 );
+	/// Detect edges using canny
+	Canny( data->bin, canny_output, CANNY_THRESHOLD, CANNY_THRESHOLD*2, 3 );
 
-  /// Find contours
-  findContours( canny_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
+	/// Find contours
+	findContours( canny_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
 
-  /// Approximate contours to polygons + get bounding rects and circles
-  vector<vector<Point> > contours_poly( contours.size() );
-  vector<Rect> boundRect( contours.size() );
-  vector<Point2f>center( contours.size() );
-  vector<float>radius( contours.size() );
+	/// Approximate contours to polygons + get bounding rects and circles
+	vector<vector<Point> > contours_poly( contours.size() );
+	vector<Rect> boundRect( contours.size() );
+	vector<Point2f>center( contours.size() );
+	vector<float>radius( contours.size() );
 
-  for( int i = 0; i < contours.size(); i++ )
-     { approxPolyDP( Mat(contours[i]), contours_poly[i], 3, true );
-       boundRect[i] = boundingRect( Mat(contours_poly[i]) );
-       minEnclosingCircle( (Mat)contours_poly[i], center[i], radius[i] );
-     }
+	for( int i = 0; i < contours.size(); i++ )
+	{ approxPolyDP( Mat(contours[i]), contours_poly[i], 3, true );
+	boundRect[i] = boundingRect( Mat(contours_poly[i]) );
+	minEnclosingCircle( (Mat)contours_poly[i], center[i], radius[i] );
+	}
 
-  /// Draw polygonal contour + bonding rects + circles
-  Mat drawing = Mat::zeros( canny_output.size(), CV_8UC3 );
-  Mat aux_resize;
-  for( int i = 0; i< contours.size(); i++ ){
-		if(boundRect[i].area()>BOUNDING_AREA){
+	/// Draw polygonal contour + bonding rects + circles
+	Mat drawing;
+	src.copyTo(drawing);
+
+	Mat aux_resize;
+	for( int i = 0; i< contours.size(); i++ ){
+		if(boundRect[i].area()>BOUNDING_AREA && !((boundRect[i].height > (boundRect[i].width * 1.10)) || (boundRect[i].height < (boundRect[i].width * 0.90)))){
 			Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
 			drawContours( drawing, contours_poly, i, color, 1, 8, vector<Vec4i>(), 0, Point() );
 			rectangle( drawing, boundRect[i].tl(), boundRect[i].br(), color, 2, 8, 0 );
-			cout << boundRect[i] <<endl;
-			//strcpy(buf,"Croped ");strcat(buf,data->filter_name);sprintf(num,"%d",i);strcat(buf,num);
-			resize(src(boundRect[i]),aux_resize,RESIZE_XY_SIZE);
+			resize(src_puro(boundRect[i]),aux_resize,RESIZE_XY_SIZE);
 			possible_signals.push_back(aux_resize);
-             //circle( drawing, center[i], (int)radius[i], color, 2, 8, 0 );
 		}
-     }
-
-  /// Show in a window
-  //namedWindow( "Contours", CV_WINDOW_AUTOSIZE );
-  //strcpy(buf,"Contours");strcat(buf,data->filter_name);
-  //imshow(buf, drawing );
+	}
+	imshow("Drawing",drawing);waitKey(1);
 }
